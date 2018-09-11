5535fb9875aa565446a4679fa4c7a52f5e530e09
Yimeng Wang <20wangyimeng@bprep.org>
2018-09-11 09:15:56 -0700

    Unit2BLab

---
+++ Unit2BLab/Unit2BLab.py
@@ -0,0 +1,24 @@
+numGrade=input('what grade did you get? ')
+print(numGrade)
+if int(numGrade)>=90:
+    print('yahoo you got an A!')
+elif int(numGrade)>=80:
+    print('good you got a B')
+elif int(numGrade)>=70:
+    print('Okay you got a C')
+else:
+    print('you need to study more')
+color=input('what is your favorite color? ')
+print(color)
+if (color)==('blue'):
+    print('blue like the sky')
+elif (color)==('green'):
+    print('grenn like the forest')
+elif (color)==('red'):
+    print('i love red even though i ama computor that is not capable of love')
+elif (color)==('yellow'):
+    print('yuck i dont like yellow')
+else:
+    print('i am a dumb computor--i dont know more than four colors')
+
+
