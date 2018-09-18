d32ce57822126a88cf71cbe928f5f82b31a69cab
Yimeng Wang <20wangyimeng@bprep.org>
2018-09-18 09:20:33 -0700

    Unit3Lab

---
+++ Unit3Lab/Unit3Lab.py
@@ -0,0 +1,30 @@
+#def myfunc() :
+#   print('Python')
+#    print('St. Charles')
+#myfunc()
+
+#def grade():
+#    x =input('what Grade are you in ')
+#    y=int(x) - 1
+#    print(str(y))
+#grade()
+
+#def helloCity(city, grade):
+ #   print('you are from ' + city)
+ #   print('you are in ' + str(grade))
+
+#print('starts here')
+#myCity = input('where are you from? ')
+#myGrade = input('what grade are you in? ')
+#helloCity(myCity, myGrade)
+
+from random import
+
+def RandomNumber():
+    x = input('what is your start number? ')
+    y = input('what is your end number? ')
+    myNumber = randint(int(x), int(y))
+    print(str(myNumber) + 'is a number between ')
+    print(str(x) + "and" +str(y))
+
+RandomNumber()
