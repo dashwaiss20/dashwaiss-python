8c16457c7d2aba37f2dc2cda41b3d45997ff6dfb
Yimeng Wang <20wangyimeng@bprep.org>
2018-09-24 09:16:56 -0700

    Unit123Lab

---
+++ Unit123Lab/Unit123Lab.py
@@ -0,0 +1,27 @@
+def main():
+    print('Bellarmine point grade average')
+    gradeLevel = input('what grade are you in? ')
+    return (yearInSchool(LongGrade))
+    print(longGrade)
+
+
+
+
+
+
+
+def yearInSchool(gradeLevel):
+    if gradeLevel == 9:
+        longGrade = ('Freshman')
+    elif gradeLevel == 10:
+        longGrade = ('Sophmore')
+    elif gradeLevel == 11:
+        longGrade = ('Junior')
+    elif gradeLevel == 12:
+        longGrade = ('Senior')
+    else:
+        longGrade = ('no grade')
+    return (longGrade)
+main()
+
+
