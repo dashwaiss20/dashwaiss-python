cd11d2b0fa384f4f851ae4ddb671c62909a77fae
dashwaiss20 <42872507+dashwaiss20@users.noreply.github.com>
2018-09-25 22:48:07 -0700

    Update Unit123Lab.py

--- Unit123Lab/Unit123Lab.py
+++ Unit123Lab/Unit123Lab.py
@@ -1,29 +1,41 @@
 def main():
+    l = [86.5, 75.3, 98.9, 92.3]
     print('Bellarmine point grade average')
     gradeLevel = input('what grade are you in? ')
-    print ("You are in " + str(yearInSchol(gradeLevel)))
-
-
-
-
-
-
+    print ("congrats You are " + str(yearInSchool(gradeLevel)))
+    print("your current GPA is a ") + str(getGPA(l))
+    print("your current letter grade is a ") + str(getlettergrade(getGPA(l)))
 
 def yearInSchool(gradeLevel):
     if gradeLevel == 9:
-        longGrade = ('Freshman')
+        longGrade = ('a Freshman')
     elif gradeLevel == 10:
-        longGrade = ('Sophmore')
+        longGrade = ('a Sophmore')
     elif gradeLevel == 11:
         longGrade = ('Junior')
     elif gradeLevel == 12:
-        longGrade = ('Senior')
+        longGrade = ('a Senior')
     else:
         longGrade = ('no grade')
     return (longGrade)
 
+def getGPA(l):
+    l = [86.5, 75.3, 98.9, 92.3]
+    gpa = sum(l) / float(len(l))
+    return(gpa)
 
-
+def getlettergrade(getGPA):
+    if getGPA >= 90:
+        lettergrade = ("A")
+    elif getGPA >= 80:
+        lettergrade = ("B")
+    elif getGPA >= 70:
+        lettergrade = ("C")
+    elif getGPA >= 60:
+        lettergrade = ("D")
+    else:
+        lettergrade = ("F")
+    return (lettergrade)
 
 main()
 
