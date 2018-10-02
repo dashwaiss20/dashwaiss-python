4059169c6bf07fcbb27cb3fde8abb83f52247a17
Yimeng Wang <20wangyimeng@bprep.org>
2018-10-02 09:11:59 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Unit123Lab/Unit123Lab.py

---
+++ Lab4A/Unit4A.py
@@ -0,0 +1,21 @@
+def main():
+    ye = input('whats your string? ')
+    print(ye)
+    print(deVowel(ye))
+
+def deVowel(ye):
+    noVowel = ""
+    for x in ye:
+        if(x == "a" or x == "e" or x == "i" or x == "o" or x == "u" or x == "A" or x == "E" or x == "I" or x == "O" or x == "U"):
+            y = 4000
+        else:
+            noVowel = noVowel + x
+    return(noVowel)
+main()
+
+def main2():
+    list = [1,2,3,4]
+
+def mathstuff():
+
+
