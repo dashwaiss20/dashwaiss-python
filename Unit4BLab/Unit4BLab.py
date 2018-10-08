49c64ee5abc8363bc86eed4e9c8fb07671a8c95e
Yimeng Wang <20wangyimeng@bprep.org>
2018-10-08 09:12:01 -0700

    k

--- Unit4BLab/Unit4BLab.py
+++ Unit4BLab/Unit4BLab.py
@@ -1,1 +1,14 @@
+for i in range(1,6):
+    print(i)
+    answer = (i + 10)
+    skrt = (i * 10)
+    print(str(i) + (' + 10 = ') + str(answer))
+    print(str(i) + (' * 10 = ') + str(skrt))
+    print('pause')
+
+list = [10,20,30,40,50]
+print(list)
+
+for x in range(0, list):
+
 
