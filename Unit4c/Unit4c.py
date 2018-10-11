b41e585a4543eedfee375adc9c36128656977b6b
Yimeng Wang <20wangyimeng@bprep.org>
2018-10-11 09:18:30 -0700

    skrt

---
+++ Unit4c/Unit4c.py
@@ -0,0 +1,14 @@
+def draw7():
+    for x in range(0,7):
+        for s in range(0,7):
+            print('*', end=" ")
+        print()
+draw7()
+
+def starsandstripes():
+    for t in range(0,6):
+        for f in range(0,6):
+            print("-", end=)
+        print()
+
+starsandstripes()
