02c65b0c9ef494b19b86fce7fc5aff18c1a949a7
Yimeng Wang <20wangyimeng@bprep.org>
2018-10-19 09:11:18 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Unit123Lab/Unit123Lab.py

--- Unit123Lab/Unit123Lab.py
+++ Unit123Lab/Unit123Lab.py
@@ -1,15 +1,17 @@
 def main():
     num = int(input('how many? '))
-    l = [0] * num
+    grades = []
     for s in range(0, num):
-        l = input('grade')
-    length = len(l)
+        l = int(input('grade'))
+        grades.append(l)
+    print(grades)
+    gpa = getGPA(grades)
+    print(gpa)
     print('Bellarmine point grade average')
     gradelevel = input('what grade are you in? ')
     print('congrats you are ' + str(yearInSchool(gradelevel)))
-    calcGPA = getGPA(l, length)
-    grade = getlettergrade(calcGPA)
-    print('you GPA is ' + str(calcGPA))
+    grade = getlettergrade(gpa)
+    print('you GPA is ' + grade)
     print('your current letter grade is ' + str(grade))
     if grade == ('D') or grade == ('F'):
         print('you failed')
@@ -30,13 +32,13 @@ def yearInSchool(gradelevel):
         longGrade = ('not in Highschool')
     return (longGrade)
 
-def getGPA(myL,length):
+def getGPA(grades):
 #    gpa = sum(myL) / float(MyLength)
     #gpa = float("0")
     added = 0.0
-    for x in myL:
+    for x in grades:
         added += float(x)
-    gpa = (added) / int(length)
+    gpa = added / len(grades)
     return (gpa)
 
 #gpa = float(gpa + (x))
