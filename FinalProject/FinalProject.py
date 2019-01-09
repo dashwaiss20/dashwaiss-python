9fa99889feb7b6352c74fa91a3510e6785c425bf
Yimeng Wang <20wangyimeng@bprep.org>
2019-01-09 10:06:42 -0800

    this is epic?

---
+++ FinalProject/FinalProject.py
@@ -0,0 +1,14 @@
+import random
+
+def starting_Character():
+    Name = input('What would you like your name to be? ')
+    print('Hello ' + (Name))
+
+starting_Character()
+
+def Starting_Game():
+    jkjk = input('yeet?')
+    Inventory  =  ['yeet', (jkjk)]
+    print(Inventory)
+
+Starting_Game()
