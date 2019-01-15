558faccb1e2ee28746dc4dcb3a3a2f925c45f228
Yimeng Wang <20wangyimeng@bprep.org>
2019-01-15 10:46:55 -0800

    this is epic??

--- FinalProject/FinalProject.py
+++ FinalProject/FinalProject.py
@@ -12,21 +12,44 @@ def Starting_Game():
     jkjk = input('what would you like to buy?')
     print(jkjk)
     if jkjk == ('wheel'):
-        amountW = input('how many would you like to buy?')
+        amountW = input('how many would you like to buy? [1-5]')
         if amountW == ('1'):
             print("1 wheel")
+            list.append('1 wheel')
         if amountW == ('2'):
             print("2 wheels")
+            s = ('2 wheel')
         if amountW == ('3'):
             print("3 wheels")
+            s = ('3 wheel')
         if amountW == ('4'):
             print("4 wheels")
+            s = ('4 wheel')
         if amountW == ("5"):
             print('5 wheels')
+            s = ('5 wheel')
         else:
-            print('oofofofo')
+            print('hello')
 
-    Inventory  =  ['yeet', ('s')]
-    print(Inventory)
+    if jkjk == ('axel'):
+        amountA = input('how many would you lik   e to buy? [1-5]')
+        if amountA == ('1'):
+            print("1 wheel")
+            s = ('1 wheel')
+        if amountA == ('2'):
+            print("2 wheels")
+            s = ('2 wheel')
+        if amountA == ('3'):
+            print("3 wheels")
+            s = ('3 wheel')
+        if amountA == ('4'):
+            print("4 wheels")
+            s = ('4 wheel')
+        if amountA == ("5"):
+            print('5 wheels')
+            s = ('5 wheel')
+
+    list =  ['yeet']
+    print(list)
 
 Starting_Game()
